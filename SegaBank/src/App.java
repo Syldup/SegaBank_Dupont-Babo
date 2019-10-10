c460ec4d02368fd1b41aedfa74613a5e9aaed7da
Samuel <babo.samuel0@gmail.com>
2019-10-10 09:41:28 +0200

    [ADD]: added menu functionalities

--- SegaBank/src/App.java
+++ SegaBank/src/App.java
@@ -1,6 +1,15 @@
+import bo.Agence;
+import bo.Menu;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
 
-    public static void main(String[] args) {
+    private static Scanner sc = new Scanner( System.in );
 
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.mainMenu();
     }
 }
